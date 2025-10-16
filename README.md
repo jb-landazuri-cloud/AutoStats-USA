4b0ec9c56dc4e9b94222b0f36b70f631ab2ef4ef
jubeml <jwalkerlandazuri@gmail.com>
2025-10-16 11:27:49 -0600

    Histograma y Dispersión

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 7mo_proyecto
+Esta app sirve para generar dos diferentes tipos de estadisticos de los "Anuncios de Automioviles en USA" cuando el usuario le de click al boton correspondiente a cada uno, dichos diagramas posibles por el momento son de ser generados son:
+
+1.- Histograma de los Anuncios de Automoviles en USA
+2.- Diagrama de Dispersión de los Anuncios de Automioviles en USA
+
+Condichos diagramas se puede analizar el comportamiento de los Anuncios de Automioviles en USA respecto a...
---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df_vehicles_us = pd.read_csv('vehicles_us.csv')
+
+st.header('Estadisticas de Vehículos en EE.UU.')  # Encabezado
+
+# Creación del Botón del histograma
+hist_button = st.button('Mostrar Histograma')
+
+if hist_button:  # Si se hace click en el Botón
+    # Se muestra este mensaje
+    st.write('Histograma de Anuncios de Venta de Coches')
+
+    # Se crea un histograma
+    fig = px.histogram(df_vehicles_us, x="Odometro",
+                       title='Histograma de Vehículos en EE.UU.')
+
+    # Se muestra un Plotly interactivo del histograma
+    st.plotly_chart(fig, use_container_width=True)
+
+####################################################################################################################
+
+    # Creación del Botón del Diagrama de Dispersión
+    disp_button = st.button('Mostrar Diagrama de Dispersión')
+
+if hist_button:  # Si se hace click en el Botón
+    # Se muestra este mensaje
+    st.write('Diagrama de Dispersión de Anuncios de Venta de Coches')
+
+    # Se crea el Diagrama de Dispersión
+    fig = px.scatter(df_vehicles_us, x='columna_x', y='columna_y',
+                     title='Diagrama de Dispersión de Vehículos en EE.UU.')
+
+    # Se muestra un Plotly interactivo del Diagrama de Dispersión
+    st.plotly_chart(fig, use_container_width=True)
